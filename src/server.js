b1a36e6affa43e6293d4e10bc33d5fb198a7a7d6
LoganML <34077969+LoganML@users.noreply.github.com>
2018-07-17 18:12:39 -0600

    Austind (#11)
    * get routes and data manipulation code
    
    * var set-up and db call
    
    * npm packages
    
    * comment out dates

--- src/server.js
+++ src/server.js
@@ -1,5 +1,31 @@
                     // THIS IS MONGODB STUFF AD
-                    //7/15/18
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var cheerio = require("cheerio");
+var path = require("path");
+var app = express();
+var PORT = process.env.PORT || 4000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("./public"));
+
+// connect to database
+mongoose.Promise = Promise;
+var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/mDB";
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect(dbConnect);
+}
+                    
+                    // 7/15/18
 db = connect('127.0.0.1:27017/mDB') 
 
     function insertData(myObject){
@@ -12,26 +38,25 @@ db = connect('127.0.0.1:27017/mDB')
             printjson(cursor.next());
         }
     }
-                    //7/15/18
                     
-// import { 
-//     Stitch,
-//     RemoteMongoClient,
-//     AnonymousCredential
-// } from "mongodb-stitch-browser-sdk";
+import { 
+    Stitch,
+    RemoteMongoClient,
+    AnonymousCredential
+} from "mongodb-stitch-browser-sdk";
 
-// const client = Stitch.initializeDefaultAppClient('doors_to_minutes-ggkln');
+const client = Stitch.initializeDefaultAppClient('doors_to_minutes-ggkln');
 
-// const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb1').db('<DATABASE>');
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb1').db('<mDB>');
 
-// client.auth.loginWithCredential(new AnonymousCredential()).then(user => 
-//   db.collection('<COLLECTION>').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
-// ).then(() => 
-//   db.collection('<COLLECTION>').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
-// ).then(docs => {
-//     console.log("Found docs", docs)
-//     console.log("[MongoDB Stitch] Connected to Stitch")
-// }).catch(err => {
-//     console.error(err)
+client.auth.loginWithCredential(new AnonymousCredential()).then(user => 
+  db.collection('<COLLECTION>').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
+).then(() => 
+  db.collection('<COLLECTION>').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
+).then(docs => {
+    console.log("Found docs", docs)
+    console.log("[MongoDB Stitch] Connected to Stitch")
+}).catch(err => {
+    console.error(err)
 // });
                     // THIS IS MONGODB STUFF AD
