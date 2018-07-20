6b8c751496e793168798b6afeef3a011e3430230
LoganML <34077969+LoganML@users.noreply.github.com>
2018-07-19 21:30:58 -0600

    mysql (#22)

---
+++ schema.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE doors;
+USE doors;
+CREATE TABLE Monday1 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Tuesday1 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Wednesday1 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Thursday1 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Friday1 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Monday2 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Tuesday2 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Wednesday2 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Thursday2 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+CREATE TABLE Friday2 (
+item_id int not null AUTO_INCREMENT,
+interior integer(50) not null,
+exterior integer(50) not null,
+steel_fiber integer(50) not null,
+cut integer(50) not null,
+primary key (item_id)
+);
+
+select * from doors;
+INSERT INTO week ()
+VALUES  (Monday, )
+        
